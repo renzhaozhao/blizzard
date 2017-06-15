44dac8c013c3c71a2dfdb45cb79df43c67ff69fd
renzhao <renzhao@vpgame.cn>
2017-06-15 15:55:06 +0800

    修改view地址

--- app/controllers/UserCtroller.js
+++ app/controllers/UserCtroller.js
@@ -1,14 +1,37 @@
 import UserModel from '../models/UserModel'
 
 export default {
-  create: async ctx => {
-    ctx.body = await UserModel.create({
-      name: ctx.request.query.name,
-      age: ctx.request.query.age
-    })
+  create: async (ctx, next) => {
+    try {
+      ctx.body = await UserModel.create({
+        name: ctx.request.query.name,
+        age: ctx.request.query.age
+      })
+    } catch (err) {
+      next(err)
+    }
   },
 
-  list: async ctx => {
-    ctx.body = await UserModel.find()
+  list: async (ctx, next) => {
+    try {
+      ctx.body = await UserModel.find()
+    } catch (err) {
+      next(err)
+    }
+  },
+
+  findOne: async (ctx, next) => {
+    try {
+      const user = await UserModel.find({ _id: ctx.params.id })
+      if (user.length) {
+        ctx.body = user[0]
+      } else {
+        ctx.body = {
+          massage: '没有找到'
+        }
+      }
+    } catch (err) {
+      next(err)
+    }
   }
 }
